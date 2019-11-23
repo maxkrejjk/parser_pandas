9123c8b69c64c3e04bc501062614dbfd36456f31
Maks Koreiko <max.krejjk@yandex.ru>
2019-11-23 13:36:45 +0300

    idws working e lasts

--- optimized.py
+++ optimized.py
@@ -1,12 +1,12 @@
-import pandas
 import argparse
+import re
 from pathlib import Path
 from time import perf_counter
 
+import pandas
+
 parser = argparse.ArgumentParser(description='Log parser')
-parser.add_argument('method', type=str, help='Parse method (i,d,e,w,s)  like idc')
 parser.add_argument('infile', type=Path, help='Log filename')
-parser.add_argument('outfile', type=Path, help='Output filename')
 argv = parser.parse_args()
 start = perf_counter()
 if __name__ == '__main__':
@@ -21,7 +21,7 @@ if __name__ == '__main__':
                           encoding='utf-8')
     logged = csv[csv['token'] != '-'].rename(columns={'dur_ttl': 'l_dur_ttl', 'dur_outer': 'l_dur_outer'})
 
-    # Code by date
+    # Code by Date
     by_date = csv.resample('D', on='date').sum().join(logged.resample('D', on='date').sum())
     by_date.fillna(0).to_csv('by_date.csv', sep=';')
 
@@ -33,13 +33,31 @@ if __name__ == '__main__':
 
     # Code by Week
     by_week_df = pandas.DataFrame(columns=['dow'])
-    by_week = csv.resample('30T', on='date').agg(['sum', 'median', 'mean','count'])
+    by_week = csv.resample('30T', on='date').agg(['sum', 'median', 'mean', 'count'])
     by_week['dow'] = by_week.index.weekday
     by_week.index = by_week.index.time
     by_week['time'] = by_week.index
     by_week.sort_values(by=['dow',
-                            'time']).drop(columns=['count'], level=1).fillna(0).to_csv('by_week.csv', sep=';')
-    print(by_week.info())
-    print(csv.info())
+                            'time']).groupby(['dow', 'time']).sum().fillna(0).to_csv('by_week.csv', sep=';')
+
+    # Code by Session
+    csv['date_start'] = csv['date']
+    csv['date_end'] = csv['date']
+    csv['count'] = csv['method']
+    by_session = csv.groupby('token')['date_start', 'date_end',
+                                      'method', 'dur_ttl', 'dur_outer'].agg({'date_start': 'min', 'date_end': 'max',
+                                                                             'method': 'count', 'dur_ttl': 'sum',
+                                                                             'dur_outer': 'sum'}).to_csv('by_session'
+                                                                                                         '.csv',
+                                                                                                         sep=';')
+
+    # Code by Endpoint
+    def regex(string):
+        return re.findall(r'^http:\/\/[a-z]+.+\/+[a-z]+\/', string)
+
+    csv['endpoint'] = csv['url'].apply(regex)
+    print(csv.groupby('endpoint')['dur_ttl', 'dur_outer'])
+    #csv['endpoint'] =
+    #csv.groupby('endpoint').to_csv('endpoint.csv', sep=';')
     print('Done in :', perf_counter() - start)
     exit(0)
